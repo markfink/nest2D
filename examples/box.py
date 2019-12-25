031338a96bb69746911ab9622216bcce67e831b1
mark <mark@mark-fink.de>
2019-12-25 17:11:04 +0100

    started work on Box

--- examples/box.py
+++ examples/box.py
@@ -2,11 +2,11 @@ import nest2d
 
 
 def main():
-    p = nest2d.Point(-5000000, 8954050)
-    print(f"repr: {repr(p)}")
+    b = nest2d.Box(150000000, 150000000)
+    print(f"repr: {repr(b)}")
     #print(f"x: {p.x}")  # TODO
     #print(f"y: {p.y}")
-    print(nest2d.Point.__doc__)
+    print(nest2d.Box.__doc__)
 
 
 if __name__ == '__main__':
--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ int add(int i, int j) {
 namespace py = pybind11;
 
 using Point = libnest2d::Point;
+using Box = libnest2d::Box;
 
 PYBIND11_MODULE(nest2d, m)
 {
@@ -35,4 +36,26 @@ PYBIND11_MODULE(nest2d, m)
                 return p == q;
             }
         );
+
+    py::class_<Box>(m, "Box", "2D Box point pair")
+        .def(py::init<int, int>(),  py::arg("p1"), py::arg("p2"))
+        //.def_property_readonly("x", &Point::X) // TODO
+        //.def_property_readonly("y", &Point::Y)
+        /*.def("__repr__",
+             [](const Box &b) {
+                 std::string r("Box(");
+                 r += boost::lexical_cast<std::string>(b.p1);
+                 r += ", ";
+                 r += boost::lexical_cast<std::string>(b.p2);
+                 r += ")";
+                 return r;
+             }
+        )
+        .def("__eq__",
+            [](const Box &b, const Box & c) {
+                return b == c;
+            }
+        )*/
+        ;
+
 }
