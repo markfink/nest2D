6563a1cb7f551d54ca97f2b98f52a56d9a56bc1d
mark <mark@mark-fink.de>
2019-12-25 16:54:29 +0100

    docstring

--- src/main.cpp
+++ src/main.cpp
@@ -10,34 +10,26 @@ int add(int i, int j) {
 
 namespace py = pybind11;
 
-
-//using Point = CDT::Point;
-//using Point = libnest2d::PointImpl;
 using Point = libnest2d::Point;
 
 PYBIND11_MODULE(nest2d, m)
 {
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
+    m.doc() = "2D irregular bin packaging and nesting for python";
 
-        Some other explanation about the add function.
-    )pbdoc");
-
-    py::class_<Point>(m, "Point")
+    py::class_<Point>(m, "Point", "2D Point")
         .def(py::init<int, int>(),  py::arg("x"), py::arg("y"))
-        //.def(py::init<double, double>(), py::arg("x"), py::arg("y"))
-        //.def_property_readonly("x", &Point::x)
-        //.def_property_readonly("y", &Point::y)
-        /*.def("__repr__",
+        //.def_property_readonly("x", &Point::X) // TODO
+        //.def_property_readonly("y", &Point::Y)
+        .def("__repr__",
              [](const Point &p) {
                  std::string r("Point(");
-                 r += boost::lexical_cast<std::string>(p.x());
+                 r += boost::lexical_cast<std::string>(p.X);
                  r += ", ";
-                 r += boost::lexical_cast<std::string>(p.y());
+                 r += boost::lexical_cast<std::string>(p.Y);
                  r += ")";
                  return r;
              }
-        )*/
+        )
         .def("__eq__",
             [](const Point &p, const Point & q) {
                 return p == q;
